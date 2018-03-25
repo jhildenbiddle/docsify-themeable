572211a2fc791b8ee2ba6712dd70eb3ac9a9c961
John Hildenbiddle <john@hildenbiddle.com>
2018-03-25 13:27:50 -0700

    Fix for browsersync and alternate stylesheets

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const compression = require('compression');
 
 browserSync.init({
     files: [
-        'dist/**/*.*',
-        'docs/**/*.*'
+        './dist/**/*.*',
+        './docs/**/*.*'
     ],
     ghostMode: {
         clicks: false,
@@ -19,14 +19,49 @@ browserSync.init({
     reloadOnRestart: true,
     server: {
         baseDir: [
-            './docs'
+            './docs/'
         ],
         middleware: [
             compression()
-        ],
-        routes: {
-            '/css': './dist/css',
-            '/js' : './dist/js'
+        ]
+    },
+    serveStatic: [
+        './dist/'
+    ],
+    snippetOptions: {
+        rule: {
+            match: /<\/body>/i,
+            fn: function (snippet, match) {
+                // Force enable stylesheet (required for some browsers)
+                const styleSwitchFix = `
+                    <script>
+                        var browsersyncObserver = new MutationObserver(function(mutationsList) {
+                            for (var mutation of mutationsList) {
+                                for (var node of mutation.addedNodes) {
+                                    var isStylesheet = node.tagName === 'LINK' && node.hasAttribute('rel') && node.getAttribute('rel').indexOf('stylesheet') !== -1;
+
+                                    if (isStylesheet) {
+                                        var isAlternate = node.getAttribute('rel').indexOf('alternate') !== -1;
+                                        var isEnabled   = !node.disabled || node.getAttribute('rel').indexOf('alternate') === -1;
+
+                                        if (!isAlternate && isEnabled) {
+                                            node.disabled = true;
+                                            node.disabled = false;
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        browsersyncObserver.observe(document.documentElement, {
+                            childList: true,
+                            subtree: true
+                        });
+                    </script>
+                `;
+
+                return snippet + styleSwitchFix + match;
+            }
         }
     },
     rewriteRules: [
