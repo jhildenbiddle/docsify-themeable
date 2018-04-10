017e484636207ad65294761046128684a82609f4
John Hildenbiddle <john@hildenbiddle.com>
2018-04-09 21:46:13 -0700

    Added responseTables check

--- src/js/plugin-responsive-tables.js
+++ src/js/plugin-responsive-tables.js
@@ -3,48 +3,52 @@
 // Adds markdown table renderer for use with responsive table CSS
 export default function(hook, vm) {
     hook.init(function() {
-        const markdown = window.$docsify.markdown = window.$docsify.markdown || {};
-        const renderer = markdown.renderer = markdown.renderer || {};
+        const isEnabled = ((window.$docsify || {}).themeable || {}).responsiveTables !== false;
 
-        markdown.smartypants = markdown.smartypants || true;
-        renderer.table = renderer.table || function(header, body) {
-            let tableHtml = `
-                <div class="table-wrapper">
-                    <table>
-                        <thead>${header}</thead>
-                        <tbody>${body}</tbody>
-                    </table>
-                </div>`;
+        if (isEnabled) {
+            const markdown = window.$docsify.markdown = window.$docsify.markdown || {};
+            const renderer = markdown.renderer = markdown.renderer || {};
 
-            try {
-                const div        = document.createElement('div');
-                const styleElm   = document.head.appendChild(document.createElement('style'));
-                const styleSheet = styleElm.sheet;
-                const tableId    = '_' + Math.random().toString(36).substr(2, 9);
+            markdown.smartypants = markdown.smartypants || true;
+            renderer.table = renderer.table || function(header, body) {
+                let tableHtml = `
+                    <div class="table-wrapper">
+                        <table>
+                            <thead>${header}</thead>
+                            <tbody>${body}</tbody>
+                        </table>
+                    </div>`;
 
-                div.innerHTML = tableHtml;
+                try {
+                    const div        = document.createElement('div');
+                    const styleElm   = document.head.appendChild(document.createElement('style'));
+                    const styleSheet = styleElm.sheet;
+                    const tableId    = '_' + Math.random().toString(36).substr(2, 9);
 
-                const tableElm = div.querySelector('table');
-                const thElms   = Array.apply(null, tableElm.getElementsByTagName('th'));
-                const thTitles = thElms.map(function(thElm) {
-                    return thElm.innerHTML.replace('&nbsp;', ' ');
-                });
+                    div.innerHTML = tableHtml;
 
-                thTitles.forEach((title, i) => {
-                    const rule = `#${tableId} td:nth-child(${i + 1})::before{content:"${title}";}`;
+                    const tableElm = div.querySelector('table');
+                    const thElms   = Array.apply(null, tableElm.getElementsByTagName('th'));
+                    const thTitles = thElms.map(function(thElm) {
+                        return thElm.innerHTML.replace('&nbsp;', ' ');
+                    });
 
-                    styleSheet.insertRule(rule, styleSheet.cssRules.length);
-                });
+                    thTitles.forEach((title, i) => {
+                        const rule = `#${tableId} td:nth-child(${i + 1})::before{content:"${title}";}`;
 
-                tableElm.id = tableId;
-                tableHtml = div.innerHTML;
-            }
-            catch(e) {
-                // eslint-disable-next-line
-                console.log('Failed to render responsive table: ' + e);
-            }
+                        styleSheet.insertRule(rule, styleSheet.cssRules.length);
+                    });
 
-            return tableHtml;
-        };
+                    tableElm.id = tableId;
+                    tableHtml = div.innerHTML;
+                }
+                catch(e) {
+                    // eslint-disable-next-line
+                    console.log('Failed to render responsive table: ' + e);
+                }
+
+                return tableHtml;
+            };
+        }
     });
 }
