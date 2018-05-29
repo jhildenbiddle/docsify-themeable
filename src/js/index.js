f2e3422a8b633782cc6a230fbcf61857543a58b5
John Hildenbiddle <john@hildenbiddle.com>
2018-05-28 23:28:35 -0700

    Add util to themeable object

--- src/js/index.js
+++ src/js/index.js
@@ -14,16 +14,18 @@ import { version as pkgVersion }  from '../../package.json';
 
 // Main
 // =============================================================================
-// CSS custom properties ponyfill
-cssVars({
-    // Force ponyfill on Edge 15/16 due to numerous custom property bugs
-    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/?page=1&q=%22custom%20property%22%20%22custom%20properties%22%20%22css%20variable%22%20%22css%20variables%22
-    onlyLegacy: !(/Edge\/15|Edge\/16./i.test(navigator.userAgent)),
-    silent    : true
-});
-
 // Docsify plugins
 if (window) {
+    const cssVarsOptions = {
+        // Force ponyfill on Edge 15/16 due to numerous custom property bugs
+        // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/?page=1&q=%22custom%20property%22%20%22custom%20properties%22%20%22css%20variable%22%20%22css%20variables%22
+        onlyLegacy: !(/Edge\/15|Edge\/16./i.test(navigator.userAgent)),
+        silent    : true
+    };
+
+    // CSS custom properties ponyfill
+    cssVars(cssVarsOptions);
+
     window.$docsify = window.$docsify || {};
     window.$docsify.themeable = window.$docsify.themeable || {};
 
@@ -41,4 +43,11 @@ if (window) {
 
     // Add themeable properties
     window.$docsify.themeable.version = pkgVersion;
+
+    // Add utilities
+    window.$docsify.themeable.util = {
+        cssVars: function(options = cssVarsOptions) {
+            cssVars(options);
+        }
+    };
 }
