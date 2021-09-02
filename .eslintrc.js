788022f82e542cb4fed85a0675d779b9d1a4953d
John Hildenbiddle <jhildenbiddle@users.noreply.github.com>
2021-09-02 16:13:58 -0500

    Remove line-break lint check

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,6 @@ module.exports = {
         'dot-notation'               : ['error'],
         'eqeqeq'                     : ['error'],
         'indent'                     : ['error', 4],
-        'linebreak-style'            : ['error', 'unix'],
         'no-console'                 : ['warn'],
         'no-floating-decimal'        : ['error'],
         'no-implicit-coercion'       : ['error'],
