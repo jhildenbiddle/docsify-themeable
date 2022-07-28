f8697e90882f9716d895bc70fa52b4325383b030
John Hildenbiddle <john@hildenbiddle.com>
2022-07-28 15:03:24 -0500

    Fix CDN replacement URL regex

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ browserSync.init({
             // Ex1: //cdn.com/package-name
             // Ex2: http://cdn.com/package-name@1.0.0
             // Ex3: https://cdn.com/package-name@latest
-            match  : /(?:https?:)*\/\/.*docsify-themeable[@\d.latest]*(?=["'])/g,
+            match  : /(?:https?:)*\/\/.*cdn.*docsify-themeable[@\d.latest]*(?=["'])/g,
             replace: '/js/docsify-themeable.min.js'
         },
         {
@@ -52,7 +52,7 @@ browserSync.init({
             // Ex1: //cdn.com/package-name/path/file.js => /path/file.js
             // Ex2: http://cdn.com/package-name@1.0.0/dist/file.js => /dist/file.js
             // Ex3: https://cdn.com/package-name@latest/dist/file.js => /dist/file.js
-            match  : /(?:https?:)*\/\/.*docsify-themeable[@\d.latest]*\/(?:dist\/)/g,
+            match  : /(?:https?:)*\/\/.*cdn.*docsify-themeable[@\d.latest]*\/(?:dist\/)/g,
             replace: '/'
         }
     ]
